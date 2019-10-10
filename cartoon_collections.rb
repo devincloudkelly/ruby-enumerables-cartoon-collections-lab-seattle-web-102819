d83157ae12e23423a5938ca967798c197461288e
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-10 17:36:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,5 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < cheese_types.length
-  
+  return cheese_types[i] if array.include?
 end
