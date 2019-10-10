9ad33c5c1d0950d0da20c719e78e84daac6adc7c
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-10 17:34:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  numbered_dwarves = []
+  i = 0 
+  while i < dwarves.length 
+  numbered_dwarves.push("#{i +1}. #{dwarves[i]}")
+  i += 1
+end
+puts numbered_dwarves
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect { |call| call.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? {|calls| calls.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if !array.include?(cheese_types)
+    return nil
+  else 
+   puts "Need to figure this part out"
+   # use .detect or .find to print the first item that matches.
+   array.find do |element|
+    i = 0 
+    while i < array.length
+    element == array[i]
+    i += 1
+  end
+ 
 end
